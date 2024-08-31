732f3c35d71ee2dfc4846c059f50e6865544ac38
Suleman <sulemankarigar1412@gmail.com>
2024-08-31 17:47:37 +0530

    respond with 200

--- app/main.js
+++ app/main.js
@@ -5,9 +5,11 @@ const net = require("net");
 
 // Uncomment this to pass the first stage
 const server = net.createServer((socket) => {
-  socket.on("close", () => {
-    socket.end();
-  });
+    socket.write("HTTP/1.1 200 OK\r\n\r\n");
+
+    socket.on("close", () => {
+        socket.end();
+    });
 });
 
 server.listen(4221, "localhost");
